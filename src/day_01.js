7a7e1bab8e509814e5b79ee3546d2ec13ac409c3
ccmalcom-sf <cmalcom@salesforce.com>
2023-12-01 17:04:57 -0500

    optimized day 1 solution

--- src/day_01.js
+++ src/day_01.js
@@ -3,42 +3,23 @@
  * Puzzle Description: https://adventofcode.com/2023/day/1
  */
 
-const findTotalCalibration = (lines) => {
-  //loop through strings in array
+
+export const levelOne = ({ input, lines }) => {
+  // your code here
   let total = 0;
   lines.forEach((str) => {
     //get first numerical digit of string
-    const firstDigit = parseInt(str.match(/\d/), 10);
+    const firstDigit = Number(str.match(/\d/));
     //get last numerical digit of string
-    const lastDigit = parseInt(str.match(/\d(?=\D*$)/), 10);
+    const lastDigit = Number(str.match(/\d(?=\D*$)/));
     // concat first and last didgit and push to numberArray
-    let finalNumber = parseInt(`${firstDigit}${lastDigit}`, 10);
+    let finalNumber = Number(`${firstDigit}${lastDigit}`);
     total += finalNumber;
   });
   console.log('total: ', total);
   return total;
-}
-
-export const levelOne = ({ input, lines }) => {
-  // your code here
-  return findTotalCalibration(lines);
 };
 
-// add up numbers in array
-const sum = (numbers) => numbers.reduce((acc, x) => acc + x, 0);
-
-// get first and last digit of each string in array
-const sumOfCalibrationValues = (lines, filterDigitCharsFn, mapDigitFn) =>
-  sum(
-    lines
-      //            (line) => [...line.matchAll(digitRegex)].map((match) => match[1]),
-      .map((line) => filterDigitCharsFn(line))
-      .map((digitChars) => [digitChars[0], digitChars.at(-1)])
-      //                                      (str) => wordToDigit.get(str) || str
-      .map((firstAndLast) => firstAndLast.map(mapDigitFn))
-      // combine first and last digit into a string and convert to number
-      .map((digits) => Number(digits.join("")))
-  );
 
 
 export const levelTwo = ({ lines }) => {
@@ -54,11 +35,16 @@ export const levelTwo = ({ lines }) => {
     ["eight", "8"],
     ["nine", "9"],
   ]);
-  return sumOfCalibrationValues(
-    lines,
-    // spread string into array of characters, filter with regex(returns [match, firstCaptureGroup]), return array of firstCaptureGroup
-    (line) => [...line.matchAll(digitRegex)].map((match) => match[1]),
-    // map each string to its digit or return the string if it is not a word
-    (str) => wordToDigit.get(str) || str
-  );
+
+  let total = 0;
+
+  lines.forEach((line) => {
+    const digitChars = [...line.matchAll(digitRegex)].map((match) => match[1]);
+    const firstAndLast = [digitChars[0], digitChars.at(-1)];
+    const digits = firstAndLast.map((str) => wordToDigit.get(str) || str);
+    const finalNumber = Number(digits.join(""));
+    total += finalNumber;
+  });
+
+  return total;
 };
